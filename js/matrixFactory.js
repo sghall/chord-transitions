ce9532fb9f6f6dafc747a9b93f5a213ea7d5e934
shall <hall.steven.g@gmail.com>
2014-11-17 18:45:02 -0800

    update data and teak logic on filters

--- js/matrixFactory.js
+++ js/matrixFactory.js
@@ -1,115 +1,116 @@
 angular.module('app').factory('matrixFactory', [function () {
 
   var chordMatrix = function () {
-    var _id = 0, _keys = {}, _store = [];
 
-    var _filter = function () {};
-    var _reduce = function () {};
+    var _matrix = [], dataStore = [], _id = 0;
+    var matrixIndex = [], indexKeys = {};
+    var chordLayout, layoutCache;
 
-    var _matrix, _index, _layout, _cache;
+    var filter = function () {};
+    var reduce = function () {};
 
     var matrix = {};
 
     matrix.update = function () {
       _matrix = [], recs = [], entry = {};
 
-      _cache = {groups: {}, chords: {}};
+      layoutCache = {groups: {}, chords: {}};
 
-      _layout.groups().forEach(function (group) {
-        _cache.groups[_index[group.index]] = group;
+      chordLayout.groups().forEach(function (group) {
+        layoutCache.groups[matrixIndex[group.index]] = group;
       });
 
-      _layout.chords().forEach(function (chord) {
-        chord.source._id = _index[chord.source.index];
-        chord.target._id = _index[chord.target.index];
-        _cache.chords[chordID(chord)] = chord;
+      chordLayout.chords().forEach(function (chord) {
+        chord.source._id = matrixIndex[chord.source.index];
+        chord.target._id = matrixIndex[chord.target.index];
+        layoutCache.chords[chordID(chord)] = chord;
       });
 
-      _index = Object.keys(_keys);
+      matrixIndex = Object.keys(indexKeys);
 
-      for (var i = 0; i < _index.length; i++) {
+      for (var i = 0; i < matrixIndex.length; i++) {
         if (!_matrix[i]) {
           _matrix[i] = [];
         }
-        for (var j = 0; j < _index.length; j++) {
-          recs = _store.filter(function (row) {
-            return _filter(row, _keys[_index[i]], _keys[_index[j]]);
+        for (var j = 0; j < matrixIndex.length; j++) {
+          recs = dataStore.filter(function (row) {
+            return filter(row, indexKeys[matrixIndex[i]], indexKeys[matrixIndex[j]]);
           });
-          entry = _reduce(recs, _keys[_index[i]], _keys[_index[j]]);
+          entry = reduce(recs, indexKeys[matrixIndex[i]], indexKeys[matrixIndex[j]]);
           entry.valueOf = function () { return +this.value };
           _matrix[i][j] = entry;
         }
       }
-      _layout.matrix(_matrix);
+      chordLayout.matrix(_matrix);
       return _matrix;
     };
 
     matrix.data = function (data) {
-      _store = data;
+      dataStore = data;
       return this;
     };
 
     matrix.filter = function (func) {
-      _filter = func;
+      filter = func;
       return this;
     };
 
     matrix.reduce = function (func) {
-      _reduce = func;
+      reduce = func;
       return this;
     };
 
-    matrix.layout = function (d3_layout) {
-      _layout = d3_layout;
+    matrix.layout = function (d3chordLayout) {
+      chordLayout = d3chordLayout;
       return this;
     };
 
     matrix.groups = function () {
-      return _layout.groups().map(function (group) {
-        group._id = _index[group.index];
+      return chordLayout.groups().map(function (group) {
+        group._id = matrixIndex[group.index];
         return group;
       });
     };
 
     matrix.chords = function () {
-      return _layout.chords().map(function (chord) {
+      return chordLayout.chords().map(function (chord) {
         chord._id = chordID(chord);
-        chord.source._id = _index[chord.source.index];
-        chord.target._id = _index[chord.target.index];
+        chord.source._id = matrixIndex[chord.source.index];
+        chord.target._id = matrixIndex[chord.target.index];
         return chord;
       });
     };
 
     matrix.addKey = function (key, data) {
-      if (!_keys[key]) {
-        _keys[key] = {name: key, data: data || {}};
+      if (!indexKeys[key]) {
+        indexKeys[key] = {name: key, data: data || {}};
       }
     };
 
     matrix.addKeys = function (prop, func) {
-      for (var i = 0; i < _store.length; i++) {
-        if (!_keys[_store[i][prop]]) {
-          this.addKey(_store[i][prop], func ? func(_store, prop):{});
+      for (var i = 0; i < dataStore.length; i++) {
+        if (!indexKeys[dataStore[i][prop]]) {
+          this.addKey(dataStore[i][prop], func ? func(dataStore[i], prop, dataStore):{});
         }
       }
       return this;
     };
 
     matrix.resetKeys = function () {
-      _keys = {};
+      indexKeys = {};
       return this;
     };
 
     function chordID(d) {
-      var s = _index[d.source.index];
-      var t = _index[d.target.index];
+      var s = matrixIndex[d.source.index];
+      var t = matrixIndex[d.target.index];
       return (s < t) ? s + "__" + t: t + "__" + s;
     }
 
     matrix.groupTween = function (d3_arc) {
       return function (d, i) {
         var tween; 
-        var cached = _cache.groups[_index[d.index]];
+        var cached = layoutCache.groups[matrixIndex[d.index]];
 
         if (cached) {
           tween = d3.interpolate(cached, d);
@@ -129,7 +130,7 @@ angular.module('app').factory('matrixFactory', [function () {
     matrix.chordTween = function (d3_path) {
       return function (d, i) {
         var tween, groups;
-        var cached = _cache.chords[d._id];
+        var cached = layoutCache.chords[d._id];
 
         if (cached) {
           if (d.source._id !== cached.source._id){
@@ -137,10 +138,10 @@ angular.module('app').factory('matrixFactory', [function () {
           }
           tween = d3.interpolate(cached, d);
         } else {
-          if (_cache.groups) {
+          if (layoutCache.groups) {
             groups = [];
-            for (var key in _cache.groups) {
-              cached = _cache.groups[key];
+            for (var key in layoutCache.groups) {
+              cached = layoutCache.groups[key];
               if (cached._id === d.source._id || cached._id === d.target._id) {
                 groups.push(cached);
               }
@@ -178,17 +179,17 @@ angular.module('app').factory('matrixFactory', [function () {
     matrix.read = function (d) {
       var g, m = {};
       if (d.source) {
-        m.sname  = _keys[d.source._id].name;
+        m.sname  = indexKeys[d.source._id].name;
         m.sdata  = d.source.value;
         m.svalue = +d.source.value;
         m.stotal = _matrix[d.source.index].reduce(function (k, n) { return k + n; }, 0);
-        m.tname  = _keys[d.target._id].name;
+        m.tname  = indexKeys[d.target._id].name;
         m.tdata  = d.target.value;
         m.tvalue = +d.target.value;
         m.ttotal = _matrix[d.target.index].reduce(function (k, n) { return k + n; }, 0);
       } else {
-        g = _keys[_index[d.index]];
-        m.gname  = _keys[_index[d.index]].name;
+        g = indexKeys[matrixIndex[d.index]];
+        m.gname  = g.name;
         m.gdata  = g.data;
         m.gvalue = d.value;
       }
